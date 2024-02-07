e5523c48fba16355778ce11762cf6d738ded41a8
Michał <130533795+MichalWachura@users.noreply.github.com>
2024-02-07 16:09:46 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -19,3 +19,34 @@ toggleEl.addEventListener('click',(e)=>{
     }
     
 })
+
+
+function setTime(){
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const date = time.getDate();
+    const hours = time.getHours();
+    const hoursForClock = hours % 12; // USA time
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds()
+
+    hourEl.style.transform = `translate(-50%,-100%) rotate(${scale(hoursForClock,0,11,0,360)}deg)`
+    minuteEl.style.transform = `translate(-50%,-100%) rotate(${scale(minutes,0,59,0,360)}deg)`
+    secondEl.style.transform = `translate(-50%,-100%) rotate(${scale(seconds,0,59,0,360)}deg)`
+    
+
+    timeEl.innerHTML = `${hours} : ${minutes < 10 ?`0${minutes}`:minutes}`
+
+    dataEl.innerHTML = `${days[day]},${months[month]} <span class="circle">${day}</span>`
+}
+
+
+// THIS IS FORM STACKOVERFLOW
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+setTime()
+
+setInterval(setTime,1000)
